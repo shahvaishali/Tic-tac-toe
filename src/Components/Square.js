732da4af395ef3dd6233a65c5aa65a0e3cd56cb3
Vaishali Shah <shahvaishali2103@gmail.com>
2019-12-18 08:05:16 -0800

    Adding X and O

--- src/Components/Square.js
+++ src/Components/Square.js
@@ -1,25 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class Square extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      val: null
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    this.setState({
-      val: "X"
-    });
-  }
-
-  render() {
-    return (
-      <div className="square" onClick={this.handleClick}>
-        {this.state.val}
-      </div>
-    );
-  }
+export default function Square(props) {
+  return (
+    <div className="square" onClick={props.onClick}>
+      {props.value}
+    </div>
+  );
 }
