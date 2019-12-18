cc5bd3da0f0114efa1b14e4119ae0be13ea08e93
Vaishali Shah <shahvaishali2103@gmail.com>
2019-12-17 23:30:01 -0800

    Initial grid design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 import "./App.css";
-import Square from "./Components/Square";
+
+import Board from "./Components/Board";
 
 function App() {
   return (
     <div className="App">
-      <Square />
+      <Board />
     </div>
   );
 }
---
+++ src/Components/Board.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import Square from "./Square";
+
+export default class Board extends Component {
+  render() {
+    return (
+      <>
+        <div className="board-row">
+          <Square />
+          <Square />
+          <Square />
+        </div>
+        <div className="board-row">
+          <Square />
+          <Square />
+          <Square />
+        </div>
+        <div className="board-row">
+          <Square />
+          <Square />
+          <Square />
+        </div>
+      </>
+    );
+  }
+}
