4dc802a65dd4d4b963a085e345d1eb3dec73689a
unknown <shahvaishali2103@gmail.com>
2019-12-22 14:24:25 -0800

    Detect player 1's turn

--- src/Components/Game/Game.test.js
+++ src/Components/Game/Game.test.js
@@ -1,6 +1,13 @@
 import React from "react";
 import Game from "./game";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
+
 it("renders without crashing", () => {
   shallow(<Game />);
+});
+
+it("detect player 1's turn", () => {
+  const wrapper = mount(<Game />)
+  const playerOne = wrapper.find('h5.playerTurn').text()
+  expect(playerOne).toEqual('Player 1 (X)')
 });
