4b4d4573699f15c6bf4c460381652329a83216e0
unknown <shahvaishali2103@gmail.com>
2019-12-22 13:40:10 -0800

    Added unit test case for detecting index of square

--- src/Components/Board/Board.test.js
+++ src/Components/Board/Board.test.js
@@ -1,7 +1,15 @@
 import React from "react";
 import Board from "./board";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 it("renders without crashing", () => {
   let square = Array(9).fill(null);
   shallow(<Board square={square} blinker={[]}/>);
 });
+
+it('onClick event to get first index', () => {
+  let square = Array(9).fill(null);
+  const onClick = jest.fn();
+  const wrapper = mount(<Board square={square} onClick={onClick} blinker={[]}/>)
+  wrapper.find('div.square').first().simulate('click');
+  expect(onClick).toBeCalledWith(0);
+})
