b5d04a67dd9abad48e3a82b945b279bc7b12cafd
unknown <shahvaishali2103@gmail.com>
2019-12-23 08:56:25 -0800

    Added horizontal, vertical and diagonal checkwinner unit test

--- src/lib/checkwinner.test.js
+++ src/lib/checkwinner.test.js
@@ -2,6 +2,13 @@ import { checkWinner } from "./checkwinner";
 
 describe('Check winner function', () => {
     test('Check X is a winner', () => {
+        //Diagonal test case
         expect(checkWinner(['X', 'O', 'X', 'O', 'X', 'O', 'X', '', ''])['winner']).toEqual('X')
+        
+        //Vertical test case
+        expect(checkWinner(['X', 'O', '', 'X', 'O', '', 'X', '', ''])['winner']).toEqual('X')
+        
+        //Horizontal test case
+        expect(checkWinner(['X', 'X', 'X', 'O', '', 'O', '', '', ''])['winner']).toEqual('X')
     })
 })
